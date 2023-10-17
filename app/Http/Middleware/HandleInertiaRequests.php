0c0e613d49dfb8a8b78b6eab61ceee7d79dec596
Kévin <kevin.blaise24@gmail.com>
2023-10-17 11:35:28 +0200

    add global props

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,7 +37,14 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-            //
+            'auth' => [
+                'user' => $request->user(),
+            ],
+            'route' => [
+                'location' => $request->url(),
+                'path' => $request->getPathInfo(),
+                'query' => $request->query()
+            ]
         ]);
     }
 }
