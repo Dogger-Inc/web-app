eeba9cc63ed484c2522cd73c992ef996bf80cca6
Kévin <kevin.blaise24@gmail.com>
2023-10-31 15:40:25 +0100

    Add default lang translation laravel

---
+++ lang/fr/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'failed' => 'Ces identifiants ne correspondent pas à nos enregistrements.',
+    'password' => 'Le mot de passe est incorrect',
+    'throttle' => 'Tentatives de connexion trop nombreuses. Veuillez essayer de nouveau dans :seconds secondes.',
+    'disabled' => 'Votre compte est désactivé. Veuillez l\'activer.',
+    'captcha' => 'Le captcha est incorrect, Veuillez réessayer.',
+];
