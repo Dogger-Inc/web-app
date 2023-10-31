0e84a90a1b4aba700433b324130e354a944cb3dd
Kévin <kevin.blaise24@gmail.com>
2023-10-31 16:40:56 +0100

    add seeder for dev purpose

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +12,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        if (config('app.env') === 'local') {
+            $this->call([
+
+            ]);
+
+            if (User::count() <= 0) {
+                User::create([
+                    'firstname' => 'Mr',
+                    'lastname' => 'Admin',
+                    'email' => 'admin@dogger.com',
+                    'password' => 'Pass1234'
+                ]);
+            }
+        }
     }
 }
