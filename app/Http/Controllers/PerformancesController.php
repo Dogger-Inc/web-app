c59380d5ba3050ee140408ed9d7f3da1c1f2a1bc
Kévin <kevin.blaise24@gmail.com>
2023-12-14 14:47:33 +0100

    add missing env field for performances

--- app/Http/Controllers/PerformancesController.php
+++ app/Http/Controllers/PerformancesController.php
@@ -11,12 +11,14 @@ class PerformancesController extends Controller
             'project_id' => ['required', 'integer'],
             'duration' => ['nullable', 'numeric'],
             'comment' => ['nullable', 'string', 'max:255'],
+            'env' => ['nullable', 'string', 'max:255'],
         ]);
 
         Performance::create([
             'project_id' => $data['project_id'],
             'duration' => $data['duration'],
             'comment' => $data['comment'],
+            'env' => $data['env'],
         ]);
 
         return response()->json([
