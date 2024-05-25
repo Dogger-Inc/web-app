ada63f9c1fe2646c9bf50af24d2b0034bb9515d9
KizutoFR <yanisbenamor38@gmail.com>
2024-05-25 13:31:03 +0200

    feat: add de, ja, ru and zh localisation for backend

---
+++ lang/de/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'failed' => 'Diese Anmeldeinformationen stimmen nicht mit unseren Unterlagen überein.',
+    'password' => 'Das angegebene Passwort ist falsch.',
+    'throttle' => 'Zu viele Anmeldeversuche. Bitte versuchen Sie es in :seconds Sekunden erneut.',
+    'disabled' => 'Ihr Konto ist deaktiviert. Bitte aktivieren Sie es.',
+    'captcha' => 'Das Captcha ist falsch, bitte versuchen Sie es erneut.',
+];
