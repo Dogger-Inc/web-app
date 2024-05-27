5e740e77a15100e4f3f1344c578232902ab58c21
oltean-petru <poltean33@gmail.com>
2024-05-27 22:16:08 +0200

    added avatar to messages

--- resources/js/Components/Avatar.vue
+++ resources/js/Components/Avatar.vue
@@ -1,15 +1,21 @@
-<script setup >
+<script setup>
+import { computed } from 'vue';
 import { usePage } from '@inertiajs/vue3';
-import {computed } from 'vue';
+
+const props = defineProps({
+    user: Object,
+});
 
 const user = usePage().props.auth.user;
-const initialsLetter = computed (() => {
-    return user.firstname.charAt(0).toUpperCase() + user.lastname.charAt(0).toUpperCase();
-})
-</script>
 
+const initialsLetter = computed(() => {
+    if (props.user) {
+        return props.user.firstname.charAt(0).toUpperCase() + props.user.lastname.charAt(0).toUpperCase();
+    } else {
+        return user.firstname.charAt(0).toUpperCase() + user.lastname.charAt(0).toUpperCase();ß
+    }
+});
+</script>
 <template>
-    <div class="h-10 w-10 rounded-full bg-dogger-orange-600 p-2 text-white font-bold flex items-center justify-center">
-        {{ initialsLetter }}
-    </div>
+    <div class="flex items-center justify-center w-10 h-10 p-2 font-bold text-white rounded-full bg-dogger-orange-600 whitespace-nowrap"> {{ initialsLetter }} </div>
 </template>
